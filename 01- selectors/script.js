f90f62c6c7998f2d1398ab9a3eaed603bbd4a4d4
Yoni vidal <yonivid@gmail.com>
2020-07-10 16:25:54 +0300

    blue div mission complet

---
+++ 01- selectors/script.js
@@ -0,0 +1,20 @@
+let mode = "on";
+$("#fadeBtn").on("click", () => {
+    if (mode === "on") {
+        $(".blueDiv").fadeOut(2000, () => {
+            mode = "off"
+        })
+    }
+    else if (mode === "off") {
+        $(".blueDiv").fadeIn(2000, () => {
+            mode = "on"
+        })
+
+
+    }
+
+
+})
+
+
+
